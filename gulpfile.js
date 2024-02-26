b755ab7ad4248f76fe0c4d712c72a48b45dcde30
UnbearablePro <ilucaalexandrumarian@gmail.com>
2024-02-26 06:45:42 +0200

    Gulp update clearer build paths and functions

--- gulpfile.js
+++ gulpfile.js
@@ -54,32 +54,42 @@ const paths = {
 }
 
 const ACTIONS = {
+    CONCAT: 'CONCAT',
+    UGLIFY: 'UGLIFY',
+    SIMPLE: 'SIMPLE',
+    MULTIPLE: 'MULTIPLE',
+    // CUSTOM
     LIBRARY: 'LIBRARY',
-    CONFIG: 'CONFIG',
     LAUNCHER: 'LAUNCHER',
-    FRONTEND: 'FRONTEND',
-    NONE: 'NONE'
+    FE: 'FE', // TODO: Implement a custom for FE
+    CONFIG: 'CONFIG',
 }
 const externalLibraryPaths = {
     externalLibraryLibrary: [['src/ExternalLibrary/*.js', '!src/**/Configuration/*.js'], `ExternalLibrary.js`, ACTIONS.LIBRARY]
 }
 
 const saintTestLibraryPaths = {
-    saintTestLibrary: [['src/SaintTestLibrary/**/*.js', '!src/**/Configuration/*.js'], `SaintTestLibrary.js`, ACTIONS.LIBRARY],
-    saintTest: ["src/SaintTest.js", `SaintTest.js`, ACTIONS.LAUNCHER]
+    saintTestLibrary: [['src/SaintTestLibrary/**/*.js', '!src/**/Configuration/*.js'], `SaintTestLibrary.js`, ACTIONS.CONCAT],
+    saintTest: ["src/SaintTest.js", `SaintTest.js`, ACTIONS.LAUNCHER],
+    appscriptJson: [`src/appsscript.json`, `appsscript.json`, ACTIONS.SIMPLE],
+    playground: ["src/PlayGround.js", `PlayGround.js`, ACTIONS.LAUNCHER],
+    configuration: ["src/SaintTestLibrary/Configuration/Configuration.js", "Configuration.js", ACTIONS.CONFIG]
 }
 
 const gsForceLibraryPaths = {
+    externalLibraryLibrary: [['src/ExternalLibrary/*.js', '!src/**/Configuration/*.js'], `ExternalLibrary.js`, ACTIONS.LIBRARY],
     gsForceLibrary: [["src/GSForceLibrary/**/*.js", '!src/**/Configuration/*.js'], `GSForce.js`, ACTIONS.LIBRARY]
 }
 
 const ovbrainPaths = {
-    ovbrain: [["src/OVBRAIN/**/*.js", '!src/**/Configuration/*.js'], `OVBRAIN.js`, ACTIONS.LIBRARY]
+    gsForceLibrary: [["src/GSForceLibrary/**/*.js", '!src/**/Configuration/*.js'], `GSForce.js`, ACTIONS.CONCAT],
+    externalLibraryLibrary: [['src/ExternalLibrary/*.js', '!src/**/Configuration/*.js'], `ExternalLibrary.js`, ACTIONS.CONCAT],
+    ovbrain: [["src/OVBRAIN/**/*.js", '!src/**/Configuration/*.js'], `OVBRAIN.js`, ACTIONS.SIMPLE]
 }
 
 const defaultPaths = {
     configuration: ["src/**/Configuration.js", "Configuration.js", ACTIONS.CONFIG],
-    appscriptJson: [`src/appsscript.json`, `appsscript.json`, ACTIONS.NONE],
+    appscriptJson: [`src/appsscript.json`, `appsscript.json`, ACTIONS.SIMPLE],
     playground: ["src/PlayGround.js", `PlayGround.js`, ACTIONS.LAUNCHER]
 }
 
@@ -88,9 +98,9 @@ const buildPaths = {
     saintTestLibrary: [['src/SaintTestLibrary/**/*.js', '!src/**/Configuration/*.js'], `SaintTestLibrary.js`, ACTIONS.LIBRARY],
     gsForceLibrary: [["src/GSForceLibrary/**/*.js", '!src/**/Configuration/*.js'], `GSForce.js`, ACTIONS.LIBRARY],
     ovbrain: [["src/OVBRAIN/**/*.js", '!src/**/Configuration/*.js'], `OVBRAIN.js`, ACTIONS.LIBRARY],
-    frontEnd: ["src/**/**/*.html", `FrontEnd.html`, ACTIONS.FRONTEND],
+    frontEnd: ["src/**/**/*.html", `FrontEnd.html`, ACTIONS.FE],
     configuration: ["src/**/Configuration.js", "Configuration.js", ACTIONS.CONFIG],
-    appscriptJson: [`src/appsscript.json`, `appsscript.json`, ACTIONS.NONE],
+    appscriptJson: [`src/appsscript.json`, `appsscript.json`, ACTIONS.SIMPLE],
     playground: ["src/PlayGround.js", `PlayGround.js`, ACTIONS.LAUNCHER],
     saintTest: ["src/SaintTest.js", `SaintTest.js`, ACTIONS.LAUNCHER]
 }
@@ -100,11 +110,10 @@ const buildProductionPaths = {
     externalLibraries: [['src/ExternalLibrary/*.js', '!src/**/Configuration/*.js', '!src/**/Tests/*.js'], `ExternalLibrary.js`, ACTIONS.LIBRARY],
     gsForceLibrary: [["src/GSForceLibrary/**/*.js", '!src/**/Configuration/*.js', '!src/**/Tests/*.js'], `GSForce.js`, ACTIONS.LIBRARY],
     ovbrain: [["src/OVBRAIN/**/*.js", '!src/**/Configuration/*.js', '!src/**/Tests/*.js'], `OVBRAIN.js`, ACTIONS.LIBRARY],
-    frontEnd: ["src/**/**/*.html", `FrontEnd.html`, ACTIONS.FRONTEND],
+    frontEnd: ["src/**/**/*.html", `FrontEnd.html`, ACTIONS.FE],
     configuration: ["src/**/Configuration.js", "Configuration.js", ACTIONS.CONFIG],
-    appscriptJson: [`src/appsscript.json`, `appsscript.json`, ACTIONS.NONE],
+    appscriptJson: [`src/appsscript.json`, `appsscript.json`, ACTIONS.SIMPLE],
     playground: ["src/PlayGround.js", `PlayGround.js`, ACTIONS.LAUNCHER],
-
 }
 
 //    ###################################################### 
@@ -133,7 +142,7 @@ gulp.task('d', cleanBuild);
 gulp.task('push', claspPush);
 gulp.task('p', claspPush);
 
-gulp.task('watch',  watch);
+gulp.task('watch', watch);
 
 gulp.task('links', displayLinks);
 
@@ -164,17 +173,17 @@ async function build() {
     let flagIfToBuildAll = true;
 
     // @ts-ignore
-    if (argv.external) {flagIfToBuildAll = false; buildOf(externalLibraryPaths);};
+    if (argv.external) { flagIfToBuildAll = false; buildOf(externalLibraryPaths); };
     // @ts-ignore
-    if (argv.saint) {flagIfToBuildAll = false; buildOf(saintTestLibraryPaths);};
+    if (argv.saint) { flagIfToBuildAll = false; buildOf(saintTestLibraryPaths); return};
     // @ts-ignore
-    if (argv.ovbrain) {flagIfToBuildAll = false; buildOf(ovbrainPaths);};
+    if (argv.ovbrain) { flagIfToBuildAll = false; buildOf(ovbrainPaths); };
     // @ts-ignore
-    if (argv.gsforce) {flagIfToBuildAll = false; buildOf(gsForceLibraryPaths);};
+    if (argv.gsforce) { flagIfToBuildAll = false; buildOf(gsForceLibraryPaths); };
     // @ts-ignore
-    if (argv.production) {flagIfToBuildAll = false; buildOf(buildProductionPaths);};
+    if (argv.production) { flagIfToBuildAll = false; buildOf(buildProductionPaths); };
     // @ts-ignore
-    if (argv.default) {flagIfToBuildAll = false; buildOf(defaultPaths);};
+    if (argv.default) { flagIfToBuildAll = false; buildOf(defaultPaths); };
 
     flagIfToBuildAll ? buildOf(buildPaths) : buildOf(defaultPaths);
 }
@@ -190,21 +199,25 @@ async function buildOf(buildConfig) {
             count = count + 1;
         }
         switch (action) {
-            case ACTIONS.LIBRARY: buildUglified(path, newName, count.toString()); break;
-            case ACTIONS.NONE: buildNone(path, newName); break;
-            case ACTIONS.CONFIG: buildConcat(path, newName, '8'); break;
-            case ACTIONS.LAUNCHER: buildNone(path, newName, '9'); break;
-            case ACTIONS.FRONTEND: buildFE(path, newName); break;
+
+            case ACTIONS.CONCAT: buildConcat(path, newName, count.toString());
+            case ACTIONS.UGLIFY: buildUglify(path, newName, count.toString());
+            case ACTIONS.MULTIPLE: buildSimple(path, newName, count.toString()); break;
+            case ACTIONS.SIMPLE: buildSimple(path, newName); break;
+            case ACTIONS.LAUNCHER: buildSimple(path, newName, '9'); break;
+            case ACTIONS.LIBRARY: buildUglyCat(path, newName, count.toString()); break;
+            case ACTIONS.CONFIG: buildConfiguration(path, newName, '8'); break;
+            case ACTIONS.FE: buildSimple(path, newName); break;
             default:
                 gutil.log(yellow(`No action provided for ${newName} building with path: `) + `${path}`);
-                buildNone(path, newName);
+                buildSimple(path, newName);
                 break;
         }
     });
 }
 
 //TODO: Combine all types of builds
-async function buildUglified(path, newName, count = '0') {
+async function buildUglyCat(path, newName, count = '') {
     let nrOfFiles = 0;
     gulp.src(path)
         .on('data', function (file) {
@@ -223,13 +236,13 @@ async function buildUglified(path, newName, count = '0') {
         });
 }
 
-async function buildNone(path, newName = 'NoName', count = '') {
+async function buildConcat(path, newName = 'NoName', count = '') {
     let nrOfFiles = 0;
     gulp.src(path)
         .on('data', function (data) {
             nrOfFiles += 1;
         })
-        .pipe(rename(count + newName))
+        .pipe(concat(count + newName))
         .pipe(gulp.dest(paths.dest))
         .on('end', function () {
             if (fileBuilded == 0) {
@@ -240,7 +253,47 @@ async function buildNone(path, newName = 'NoName', count = '') {
         });
 }
 
-async function buildFE(path, newName = 'NoName') {
+async function buildUglify(path, newName, count = '') {
+    let nrOfFiles = 0;
+    gulp.src(path)
+        .on('data', function (file) {
+            nrOfFiles += 1;
+            // console.log('File Name:', file.relative);
+        })
+        .pipe(rename({ dirname: '' }))
+        .pipe(rename(count + newName))
+        .pipe(uglify())
+        .pipe(gulp.dest(paths.dest))
+        .on('end', function () {
+            if (fileBuilded == 0) {
+                gutil.log(`===================== ${blue(`Building Started`)} =====================`)
+            }
+            fileBuilded = fileBuilded + 1;
+            gutil.log(`${magenta(`${fileBuilded}/${fileToBuild}`)} ${cyan(`${newName}`)} build complete! Files builded: ${magenta(`${nrOfFiles}`)}`);
+        });
+}
+
+async function buildSimple(path, newName = 'NoName', count = '') {
+    let nrOfFiles = 0;
+    gulp.src(path)
+        .on('data', function (data) {
+            nrOfFiles += 1;
+        })
+        .pipe(rename({ dirname: '' }))
+        .pipe(rename(function(path) {
+            path.basename = count + path.basename;
+        }))
+        .pipe(gulp.dest(paths.dest))
+        .on('end', function () {
+            if (fileBuilded == 0) {
+                gutil.log(`===================== ${blue(`Building started`)} =====================`)
+            }
+            fileBuilded += 1;
+            gutil.log(`${magenta(`${fileBuilded}/${fileToBuild}`)} ${cyan(`${newName}`)} build complete! Files builded: ${magenta(`${nrOfFiles}`)}`);
+        });
+}
+
+async function buildFE(path, newName = 'NoName') { //TODO: WYP
     let nrOfFiles = 0;
     gulp.src(path)
         .on('data', function (data) {
@@ -257,7 +310,7 @@ async function buildFE(path, newName = 'NoName') {
         });
 }
 
-async function buildConcat(path, newName = 'NoName', count = '') {
+async function buildConfiguration(path, newName = 'NoName', count = '') {
     let nrOfFiles = 0;
     gulp.src(path)
         .on('data', function (data) {
@@ -324,14 +377,14 @@ async function displayLinks() {
     gutil.log(`${green(`Apps Script: `)} ${GOOGLE_APPSSCRIPT}`);
 }
 
-async function openLinks(target, ) {
+async function openLinks(target,) {
     openLink(OPEN_APPSCRIPT);
     openLink(OPEN_EXCEL);
 }
 
 async function openLink(target) {
     gulp.src('./')
-    .pipe(open(target));
+        .pipe(open(target));
 }
 
 async function runTests() {
